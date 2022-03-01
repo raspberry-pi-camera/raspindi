75fb58dd8582686d79cfe865c63f687db24e2db9
Richard Franks <git@rf152.co.uk>
2022-03-01 14:25:50 +0000

    Moved to cmake for building

--- easy-setup.sh
+++ easy-setup.sh
@@ -3,7 +3,7 @@
 set -eu
 
 sudo apt update
-sudo apt install libconfig++-dev libjasper-runtime libavahi-client3
+sudo apt install libconfig++-dev libjasper-runtime libavahi-client3 cmake
 ./build.sh
 sudo ./install.sh
 
